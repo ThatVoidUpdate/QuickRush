ebe3c04c28a7b15f5ed20b9747e8aa472851a085
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2020-02-10 09:48:48 +0000

    Redone scene to have a terrain and background layer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
     [Space]
     public Collider2D TerrainCollider;
+    [Header("Graphics")]
+    public Sprite StandardSprite;
+    public Sprite Movingsprite;
 
     private Rigidbody2D rb;
-    private BoxCollider2D collider;
+    private new BoxCollider2D collider;
+    private new SpriteRenderer renderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,7 +36,27 @@ public class Player : MonoBehaviour
         {
             VerticalMovement = JumpForce;
         }
-        rb.velocity = new Vector2(HorizontalMovement * Speed * Time.deltaTime, VerticalMovement);
+        rb.velocity = new Vector2(HorizontalMovement * Speed, VerticalMovement);
+    }
 
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            renderer.sprite = Movingsprite;
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else
+        {
+            renderer.sprite = StandardSprite;
+        }
     }
 }

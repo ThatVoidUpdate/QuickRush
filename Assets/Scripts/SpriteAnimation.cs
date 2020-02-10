c84af7399695404e70beccc5e8914bd96c776edc
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-02-10 23:55:33 +0000

    Added sprite animation script. Added animation to torches, saw enemies and bee enemies

---
+++ Assets/Scripts/SpriteAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour
+{
+    public Sprite[] AnimationFrames;
+    public float SecondsPerFrame;
+
+    private SpriteRenderer renderer;
+
+    private float CurrentTime;
+    private int CurrentFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentTime += Time.deltaTime;
+        if (CurrentTime > SecondsPerFrame)
+        {
+            CurrentFrame = (CurrentFrame + 1) % AnimationFrames.Length;
+            renderer.sprite = AnimationFrames[CurrentFrame];
+            CurrentTime -= SecondsPerFrame;
+        }
+    }
+}
